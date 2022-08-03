1944ba93603032c9563683d9411902d116cdcae8
Aaron Liu <aaronliu0130@gmail.com>
2022-08-03 17:56:56 -0400

    Add KIT Scenarist v0.7.2.rc15

---
+++ Casks/kit-scenarist.rb
@@ -0,0 +1,11 @@
+cask "kit-scenarist" do
+  version "0.7.2.rc15"
+  sha256 "89b2a16ed34c9c02d7e671b19e6a42c891e18e3a6527d133fcfb032b4df81426"
+
+  url "https://kitscenarist.ru/downloads/mac/scenarist-setup-#{version}_latest.$
+  name "KIT Scenarist"
+  desc "Screenwriting software"
+  homepage "https://kitscenarist.ru/en/index.html"
+
+  app "Scenarist.app"
+end
